137822c6628c070675706553b74c491371f03d43
rossithomaseise <rossi.thomas83140@gmail.com>
2022-01-09 19:44:31 +0100

    version graphique 2

---
+++ projet_info_4a_thomas/projet_graphique/projet_bis/fenetre2.cpp
@@ -0,0 +1,63 @@
+#include "fenetre2.h"
+#include "ui_fenetre2.h"
+#include <QPixmap>
+
+fenetre2::fenetre2(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::fenetre2)
+{
+    ui->setupUi(this);
+    //setFixedSize(dw.width()*0.965,dw.height());
+    terre = new Terre();
+    //Etres.push_back(new Humain(terre));
+    //Etres.push_back(new Vache(terre));
+    Humain* hum = new Humain(terre);
+    Vache* vache = new Vache(terre);
+    //QPixmap pix("/home/polytech/build-projet_bis-Desktop-Debug/image_flore.jpg");
+    //QPixmap pix(QCoreApplication::applicationDirPath() + "/image_flore.jpg");
+    QPixmap pix("image_flore"); // pas de .jpg
+    QPixmap pix2("terre_nucleaire");
+    ui->P2_Terre_Image->setPixmap(pix2.scaled(ui->P2_Terre_Image->width(),ui->P2_Terre_Image->height(),Qt::KeepAspectRatio));
+    //ui->P3_Flore_Image->setPixmap(pix);
+    //ui->P3_Flore_Image->setPixmap(pix.scaled(500,50,Qt::KeepAspectRatio));
+   // ui->P3_Faune_Image->setPixmap(pix.scaled(500,50,Qt::KeepAspectRatio));
+
+    ui->P3_Flore->setText("Flore : "+QString::number(Flore::getNbFlore()));
+      ui->P3_Humain->setText("Humains : "+QString::number(Humain::getNbHumains()));
+      ui->P3_Vache->setText("Vaches : "+QString::number(Vache::getNbVaches()));
+      ui->P3_Faune->setText("Faune : "+QString::number(Faune::getNbFaune()));
+      ui->P3_Conifere->setText("Coniferes : "+QString::number(Conifere::getNbConiferes()));
+      ui->P3_Algue->setText("Faune : "+QString::number(Algue::getNbAlgues()));
+      ui->P2_Temps->setText("Temps : "+QString::number(terre->getTemps()));
+      ui->P3_Vache_pollution->setText("Methane : "+QString::number(dynamic_cast<Vache*>((terre->getEtresVivants())[1])->getMethane()));
+
+}
+
+void fenetre2::on_bouton_fenetre2_clicked() // bouton retour menu
+{
+    parentWidget()->show();
+    //close() automatique
+}
+
+fenetre2::~fenetre2()
+{
+    delete ui;
+}
+
+void fenetre2::on_bouton_action_clicked() // bouton passer temps
+{
+    terre->gestionTemps();
+
+    //Mise à jour affichage
+    ui->progressBar->setValue(terre->getSante());
+    //ui->P3_Flore->setText("Flore : "+QString::number(EtreVivant::getNbEtresVivants()));
+
+      ui->P3_Flore->setText("Flore : "+QString::number(Flore::getNbFlore()));
+      ui->P3_Humain->setText("Humains : "+QString::number(Humain::getNbHumains()));
+      ui->P3_Vache->setText("Vaches : "+QString::number(Vache::getNbVaches()));
+      ui->P3_Faune->setText("Faune : "+QString::number(Faune::getNbFaune()));
+      ui->P3_Conifere->setText("Coniferes : "+QString::number(Conifere::getNbConiferes()));
+      ui->P3_Algue->setText("Faune : "+QString::number(Algue::getNbAlgues()));
+      ui->P2_Temps->setText("Temps : "+QString::number(terre->getTemps()));
+      ui->P3_Vache_pollution->setText("Methane : "+QString::number(dynamic_cast<Vache*>((terre->getEtresVivants())[1])->getMethane()));
+}
